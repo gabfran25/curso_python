bbc7f8ddc56394cf1463cfaec49a84c55abb3dc4
gabfran25 <jagl30101929@gmail.com>
2025-01-29 14:02:25 -0700

    "agregando la función de graficar pend recta"

---
+++ linea/hello.py
@@ -0,0 +1,21 @@
+'''
+COMANDOS PARA CORRER EN ANACONDA
+
+ENTRAR AL PAQUETE desarrollo4
+conda activate desarrollo4
+
+PARA AYUDA O QUE TE DIGA QUE HACER
+python app.py -h
+
+PARA CORRER app.py
+python app.py
+
+PARA CORRER funciones.py
+python funciones.py
+
+PARA PONER LOS VALORES
+python app.py -m -3.5 -b 152.89
+
+INSTALAR EL PAQUETE 'MATPLOTLIB'
+conda install conda-forge::matplotlib
+'''
