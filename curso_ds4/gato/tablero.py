05d3ccda904d3fabc62893542e37946668519450
gabfran25 <jagl30101929@gmail.com>
2025-01-29 14:45:45 -0700

    agregando la función para dibujar el tablero del juego del gato

---
+++ curso_ds4/gato/tablero.py
@@ -0,0 +1,38 @@
+'''
+Tablero.py: Dibuja el tablero del juego del gato
+'''
+import random
+
+def dibuja_tablero(simbolos:dict):
+    '''
+    Dibuja el tablero del juego del gato
+    
+    tablero con números enteros
+    print(f" {simbolos[1]} | {simbolos[2]} | {simbolos[3]} ")
+    print("---+---+---")
+    print(f" {simbolos[4]} | {simbolos[5]} | {simbolos[6]} ")
+    print("---+---+---")
+    print(f" {simbolos[7]} | {simbolos[8]} | {simbolos[9]} ")
+    '''
+    
+    print(f''' 
+    {simbolos['1']} | {simbolos['2']} | {simbolos['3']}    
+    ----------
+    {simbolos['4']} | {simbolos['5']} | {simbolos['6']}    
+    ----------
+    {simbolos['7']} | {simbolos['8']} | {simbolos['9']} 
+    ''') 
+    
+if __name__ == '__main__':
+    numeros = [str(i) for i in range(1,10)]
+    dsimbolos = {x:x for x in numeros}
+    dibuja_tablero(dsimbolos)
+    x = random.choice(numeros)
+    numeros.remove(x)
+    dsimbolos[x] = 'X'
+    dibuja_tablero(dsimbolos)
+    o = random.choice(numeros)
+    numeros.remove(o)
+    dsimbolos[o] = 'O'
+    dibuja_tablero(dsimbolos)
+    print(numeros)
