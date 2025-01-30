0f219585f1d1afa57add5ca09bb6080a3c15f118
gabfran25 <jagl30101929@gmail.com>
2025-01-30 14:22:03 -0700

    agregando la función de inteligencia artificial para el juego del gato

--- curso_ds4/gato/tablero.py
+++ curso_ds4/gato/tablero.py
@@ -22,11 +22,27 @@ def dibuja_tablero(simbolos:dict):
     ----------
     {simbolos['7']} | {simbolos['8']} | {simbolos['9']} 
     ''') 
+
+def ia(simbolos:dict):
+    '''
+    Estrategia de la computadora
+    '''
+    ocupado = True
+    while ocupado is True:
+        x = random.choice(list(simbolos.keys()))
+        if simbolos[x] not in ['X','O']:
+            simbolos[x] = 'O'
+            ocupado = False
+    
+    
     
 if __name__ == '__main__':
     numeros = [str(i) for i in range(1,10)]
     dsimbolos = {x:x for x in numeros}
     dibuja_tablero(dsimbolos)
+    ia(dsimbolos)
+    dibuja_tablero(dsimbolos)
+    '''
     x = random.choice(numeros)
     numeros.remove(x)
     dsimbolos[x] = 'X'
@@ -36,3 +52,4 @@ if __name__ == '__main__':
     dsimbolos[o] = 'O'
     dibuja_tablero(dsimbolos)
     print(numeros)
+    '''
