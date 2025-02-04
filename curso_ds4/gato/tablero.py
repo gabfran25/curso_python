d93403294921a584e42a667d6d31efdef69980c4
gabfran25 <jagl30101929@gmail.com>
2025-02-04 14:46:58 -0700

    agregando la función principal del juego del gato y la verificación de ganador

--- curso_ds4/gato/tablero.py
+++ curso_ds4/gato/tablero.py
@@ -6,15 +6,7 @@ import random
 def dibuja_tablero(simbolos:dict):
     '''
     Dibuja el tablero del juego del gato
-    
-    tablero con números enteros
-    print(f" {simbolos[1]} | {simbolos[2]} | {simbolos[3]} ")
-    print("---+---+---")
-    print(f" {simbolos[4]} | {simbolos[5]} | {simbolos[6]} ")
-    print("---+---+---")
-    print(f" {simbolos[7]} | {simbolos[8]} | {simbolos[9]} ")
     '''
-    
     print(f''' 
     {simbolos['1']} | {simbolos['2']} | {simbolos['3']}    
     ----------
@@ -46,18 +38,82 @@ def usuario(simbolos:dict):
                 ocupado = False
             else:
                 print('Casilla ocupada')
+                
+def juego(simbolos:dict):
+    ''' Juego del gato '''
+    lista_combinaciones = [
+        ['1','2','3'],
+        ['4','5','6'],
+        ['7','8','9'],   
+        ['1','4','7'],
+        ['2','5','8'],
+        ['3','6','9'],
+        ['1','5','9'],
+        ['3','5','7']
+    ]
+    en_juego = True
+    dibuja_tablero(simbolos)
+    movimiento = 0
     
+    while en_juego is True:
+        usuario(simbolos)
+        dibuja_tablero(simbolos)
+        movimiento += 1
+        
+        gana = checa_winner(simbolos,lista_combinaciones)
+        if gana is not None:
+            en_juego = False
+            continue
+        
+        if movimiento >= 9:
+            en_juego = False
+            continue
+        
+        '''checa_winner(simbolos,lista_combinaciones)
+        if checa_winner(simbolos,lista_combinaciones) is not None:
+            print(f'El ganador es {checa_winner(simbolos,lista_combinaciones)}')
+            en_juego = False
+            break'''
+            
+        ia(simbolos)
+        dibuja_tablero(simbolos)
+        movimiento += 1
+        gana = checa_winner(simbolos,lista_combinaciones)
+        if gana is not None:
+            en_juego = False
+            continue
+        
+        
+        '''checa_winner(simbolos,lista_combinaciones)
+        if checa_winner(simbolos,lista_combinaciones) is not None:
+            print(f'El ganador es {checa_winner(simbolos,lista_combinaciones)}')
+            en_juego = False
+            break
+        else:
+            print('Empate')
+            en_juego = False
+            break'''
     
-    a 
+
+def checa_winner(simbolos:dict,combinaciones:list):
+    ''' Checa si hay un ganador '''
+    for c in combinaciones:
+        if simbolos[c[0]] == simbolos[c[1]] == simbolos[c[2]]:
+            return simbolos[c[0]]
+    return None
+    
+
 if __name__ == '__main__':
     numeros = [str(i) for i in range(1,10)]
     dsimbolos = {x:x for x in numeros}
+    juego(dsimbolos)
+    
+    '''
     dibuja_tablero(dsimbolos)
     ia(dsimbolos)
     dibuja_tablero(dsimbolos)
     usuario(dsimbolos)
     dibuja_tablero(dsimbolos)
-    '''
     x = random.choice(numeros)
     numeros.remove(x)
     dsimbolos[x] = 'X'
