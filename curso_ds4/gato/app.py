64a6f5df94fa3df5eb6073f46e128b81f4985368
gabfran25 <jagl30101929@gmail.com>
2025-02-06 14:23:21 -0700

    Ciclo principal de app con actualización de score

--- curso_ds4/gato/app.py
+++ curso_ds4/gato/app.py
@@ -6,13 +6,27 @@ import tablero
 
 def main():
     '''FUNCIÓN PRINICIPAL'''
+    X = {"G":0,"P":0,"E":0}
+    O = {"G":0,"P":0,"E":0}
+    score = {"X":X,"O":O}
     numeros = [str(i) for i in range(1,10)]
-    dsimbolos = {x:x for x in numeros}
-    g = tablero.juego(dsimbolos)
-    if g is not None:
-        print(f'El ganador del juego es {g}')
-    else:
-        print('Empate. Nadie gana.')
+    corriendo = True
+    while corriendo:
+        dsimbolos = {x:x for x in numeros}
+        g = tablero.juego(dsimbolos)
+        if g is not None:
+            if g == 'X':
+                X["G"] += 1
+                O["P"] += 1
+            elif g == 'O':
+                X["P"] += 1
+                O["G"] += 1
+            else:
+                X["E"] += 1
+                O["E"] += 1
+        
+        
+        
 
 if __name__ == '__main__':
     main()
