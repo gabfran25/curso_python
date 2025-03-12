3cc34a85547bc0a04cd8d01038b2ec98fc34f32b
gabfran25 <jagl30101929@gmail.com>
2025-03-12 14:50:44 -0700

    Añade plantillas HTML para la aplicación Libros Web, incluyendo páginas de inicio, búsqueda por título y despliegue por letra.

--- curso_ds4/libros_web/funciones.py
+++ curso_ds4/libros_web/funciones.py
@@ -1,43 +1,27 @@
-'''Archivo con las funciones necesarias de la aplicación LIBRO WEB'''
+''' Archivo con las funciones necesarias de la Aplicación Libro Web'''
 import csv
 
 def lee_archivo_csv(archivo:str)->list:
-    '''Lee un archivo CSV y devuelve una lista de diccionarios'''
+    ''' Lee un archivo CSV y lo convierte en una lista de diccionarios '''
     with open(archivo, "r", encoding='utf8') as f:
         return [x for x in csv.DictReader(f)]
-    
-def crea_diccionario_titulos(lista:list)->dict:
-    '''Crea un diccionario con los títulos como claves y el resto de los datos como valores'''
-    return {x['title']:x for x in lista}
-
-def busca_en_titulo(diccionario,palabra)->list:
-    '''Busca una palabra en los títulos de la lista y devuelve una lista con los resultados'''
-    lista = []
-    for titulo, libro in diccionario.items():
-        if palabra in titulo.lower():
-            lista.append(libro)
-    return lista
 
 def crea_diccionario(lista:list, llave:str)->dict:
-    '''Crea un diccionario con la llave indicada y el resto de los datos como valores'''
-    return {x[llave]:x for x in lista}
+    ''' Crea un diccionario con la palabra "llave" como clave y el resto de los datos como valores '''
+    return {x[llave].lower(): x for x in lista}
 
 def busca_en_diccionario(diccionario:dict, palabra:str)->list:
-    '''Busca una palabra en los valores del diccionario y devuelve una lista con los resultados'''
+    ''' Busca palabra en llave de la lista de diccionarios '''
     lista = []
+    palabra = palabra.lower()
     for llave, libro in diccionario.items():
         if palabra in llave.lower():
             lista.append(libro)
     return lista
 
 def libros_empiezan_con(lista:list, letra:str)->list:
-    '''Devuelve una lista con los libros que empiezan con la letra indicada'''
-    lista_resultados = []
-    for libro in lista:
-        if libro['title'].lower().startswith(letra.lower()):
-            lista_resultados.append(libro)
-    return lista_resultados
-    # return [x for x in lista if x['title'].lower().startswith(letra.lower())]
+    ''' Busca libros que empiecen con letra '''
+    return [x for x in lista if x['title'].lower().startswith(letra.lower())]
 
 if __name__ == '__main__':
     archivo_csv = 'booklist2000.csv'
@@ -46,8 +30,7 @@ if __name__ == '__main__':
     resultado = busca_en_diccionario(diccionario_libros, 'rebels')
     print(resultado)
     diccionario_autores = crea_diccionario(lista_libros,'author')
-    resultado = busca_en_diccionario(diccionario_autores, 'james')
+    resultado = busca_en_diccionario(diccionario_autores, 'Sandra')
     print(resultado)
-    resultado = libros_empiezan_con(lista_libros, 't')
-    # print(resultado)
-    print(f'Libros que empiezan con "T" : {len(resultado)}')
+    resultado = libros_empiezan_con(lista_libros, 'a')
+    print(f'Libros que empiezan con "A": {len(resultado)}')
